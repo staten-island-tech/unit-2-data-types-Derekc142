1522fc14e9d34f263b27650d6f1fb49d72c03a76
Derekc142 <DErekc142@nycstudents.net>
2024-02-02 12:26:25 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,11 +1,11 @@
-#Data Types
+""" #Data Types
 #Numbers 1, 2, 3
 def add(x,y):
     print(x + y)
 add(1,2)
 #strings "a,b,c"
 name = "Mark"
-def gretting(person):
+def greeting(person):
     print(person)
 greeting(name)
 #1 and "1" are not the same
@@ -15,9 +15,21 @@ add("1","2")
 #booLeans
 tenure = False
 def is_tenured(status):
-    if(status == True)
+    if(status == True):
         print("They have tenure")
     else:
         print("they are not tenured")
 
-is_tenured(tenure)
+is_tenured(tenure)"""
+
+x = 3
+y = float(3)
+print(x,y)
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i) 
+
+print(values[0])
+print(values[6]) """

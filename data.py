4702b97f822ec1cadb85383ff888ea1c173b6c3d
Derekc142 <DErekc142@nycstudents.net>
2024-02-02 12:15:01 -0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,23 @@
+#Data Types
+#Numbers 1, 2, 3
+def add(x,y):
+    print(x + y)
+add(1,2)
+#strings "a,b,c"
+name = "Mark"
+def gretting(person):
+    print(person)
+greeting(name)
+#1 and "1" are not the same
+add("1","2")
+#unidentified/null
+
+#booLeans
+tenure = False
+def is_tenured(status):
+    if(status == True)
+        print("They have tenure")
+    else:
+        print("they are not tenured")
+
+is_tenured(tenure)
